2b5fd033b7fa4466e70d94ba6029fa673037b506
chameliraut <55823912+chameliraut@users.noreply.github.com>
2019-09-27 08:47:58 +0530

    Update and rename SampleApp.java to helloWord.java

---
+++ helloWord.java
@@ -0,0 +1,10 @@
+package helloWord;
+
+public class helloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Git with helloWord...!!");
+	}
+
+}
