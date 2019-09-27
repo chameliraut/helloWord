a8a35df2bbb9dab9889dbaae0febb253d1fcb23e
chameliraut <55823912+chameliraut@users.noreply.github.com>
2019-09-27 15:23:39 +0530

    Update SampleApp.java

--- SampleApp.java
+++ SampleApp.java
@@ -1,10 +1,7 @@
-package sampleApp;
-
 public class SampleApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Welcome to Git with SampleApp...!!");
+		System.out.println("Welcome to Git with SampleApp...!!");
 	}
 
 }
