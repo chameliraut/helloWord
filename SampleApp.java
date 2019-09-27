37304bed34ca83871466361e0f380f880daedc32
chameliraut <55823912+chameliraut@users.noreply.github.com>
2019-09-27 08:56:34 +0530

    Add files via upload

---
+++ SampleApp.java
@@ -0,0 +1,10 @@
+package sampleApp;
+
+public class SampleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Git with SampleApp...!!");
+	}
+
+}
